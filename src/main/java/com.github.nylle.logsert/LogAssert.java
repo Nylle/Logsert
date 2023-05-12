e8f01e30f6f3beed080cf99fc55ad20312d3fd99
Jan <Nylle@users.noreply.github.com>
2023-05-12 17:20:27 +0200

    Refactor code

--- src/main/java/com.github.nylle.logsert/LogAssert.java
+++ src/main/java/com.github.nylle.logsert/LogAssert.java
@@ -2,13 +2,10 @@ package com.github.nylle.logsert;
 
 import org.assertj.core.api.AbstractAssert;
 
-import ch.qos.logback.classic.Level;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogAssert extends AbstractAssert<LogAssert, LoggerExtension> {
-    private static final String FAIL_MESSAGE = "\nExpecting log:\n  %s\nto contain:\n  %s\nbut could not find the following:\n  %s";
 
     public LogAssert(LoggerExtension actual) {
         super(actual, LogAssert.class);
@@ -24,16 +21,11 @@ public class LogAssert extends AbstractAssert<LogAssert, LoggerExtension> {
         var candidates = actual.getLogEvents().stream().filter(x -> x.getMessage().equals(message)).collect(Collectors.toList());
 
         if(candidates.isEmpty()) {
-            failWithMessage(FAIL_MESSAGE,
+            failWithMessage("\nExpecting log:\n  %s\nto contain:\n  %s\nbut could not find the following:\n  %s",
                     actual.getLogEvents().stream().map(x -> x.getMessage()).collect(Collectors.toList()),
                     List.of(message),
                     List.of(message));
         }
         return new MessageAssert(candidates, message);
     }
-
-    private static String formatMessageAndLevel(String message, Level level) {
-        return "[" + level + ", " + message + "]";
-    }
 }
-
--- src/test/java/com/github/nylle/logsert/LogAssertTest.java
+++ src/test/java/com/github/nylle/logsert/LogAssertTest.java
@@ -1,12 +1,15 @@
 package com.github.nylle.logsert;
 
+import ch.qos.logback.classic.Level;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import ch.qos.logback.classic.Level;
+import java.util.Map;
 
 import static com.github.nylle.logsert.LogAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.tuple;
 
 class LogAssertTest {
     @RegisterExtension
@@ -17,7 +20,7 @@ class LogAssertTest {
         var somethingThatLogs = new SomethingThatLogs();
         somethingThatLogs.logInfo("message");
 
-        assertThat(sut).containsMessage("message").withLevel(Level.INFO);
+        assertThat(sut).containsMessage("message");
     }
 
     @Test
@@ -26,9 +29,18 @@ class LogAssertTest {
         somethingThatLogs.logInfo("other");
 
         assertThatExceptionOfType(AssertionError.class)
-                .isThrownBy(() -> assertThat(sut).containsMessage("message").withLevel(Level.INFO))
+                .isThrownBy(() -> assertThat(sut).containsMessage("message"))
                 .withMessageContaining("Expecting log:\n  [other]\n")
                 .withMessageContaining("to contain:\n  [message]\n")
                 .withMessageContaining("but could not find the following:\n  [message]");
+    }
+
+    @Test
+    void standardListAssertionIsSupported() {
+        var somethingThatLogs = new SomethingThatLogs();
+        somethingThatLogs.logInfoWithMdcAndException("message", Map.of("key", "value"), new RuntimeException("expected for test"));
+
+        assertThat(sut.getLogEvents()).extracting("level", "message", "MDCPropertyMap", "throwableProxy.className", "throwableProxy.message")
+                .contains(tuple(Level.INFO, "message", Map.of("key", "value"), RuntimeException.class.getName(), "expected for test"));
     }
 }
