cf7a76bb82f42acb9869b3c6b6cc7e3874d1fc14
Jan <Nylle@users.noreply.github.com>
2023-05-15 13:21:08 +0200

    Fix directory names

--- src/main/java/com/github/nylle/logsert/LoggerExtension.java
+++ src/main/java/com/github/nylle/logsert/LoggerExtension.java
@@ -1,13 +1,12 @@
 package com.github.nylle.logsert;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
 
 import java.util.List;
 
@@ -16,24 +15,20 @@ public class LoggerExtension implements BeforeEachCallback, AfterEachCallback {
     private final ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
     private final Logger logger;
 
-    /**
-     * Create a log appender for the specified class
-     * @param type
-     */
     public LoggerExtension(Class<?> type) {
-        this.logger = LoggerFactory.getLogger(type);
+        this.logger = (Logger) LoggerFactory.getLogger(type);
     }
 
     @Override
     public void afterEach(ExtensionContext extensionContext) {
         listAppender.stop();
         listAppender.list.clear();
-        ((ch.qos.logback.classic.Logger)logger).detachAppender(listAppender);
+        logger.detachAppender(listAppender);
     }
 
     @Override
     public void beforeEach(ExtensionContext extensionContext) {
-        ((ch.qos.logback.classic.Logger)logger).addAppender(listAppender);
+        logger.addAppender(listAppender);
         listAppender.start();
     }
 
